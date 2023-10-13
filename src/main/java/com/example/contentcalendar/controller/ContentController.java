cafb8ad989b8134ad7a6d389ae62493416b8fc87
Carlos Paredes <cardan1990@hotmail.com>
2023-10-13 08:56:45 +0200

    Added missing @valid annotation in the controller

--- src/main/java/com/example/contentcalendar/controller/ContentController.java
+++ src/main/java/com/example/contentcalendar/controller/ContentController.java
@@ -2,6 +2,7 @@ package com.example.contentcalendar.controller;
 
 import com.example.contentcalendar.model.Content;
 import com.example.contentcalendar.repository.ContentCollectionRepository;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -32,7 +33,7 @@ public class ContentController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("")
-    public void create(@RequestBody Content content) {
+    public void create(@Valid @RequestBody Content content) {
         repository.save(content);
     }
 
