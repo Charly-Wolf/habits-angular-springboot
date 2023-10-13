eab68ab821c0f2f938ef9caf93a13b2a8e1bbe3b
Carlos Paredes <cardan1990@hotmail.com>
2023-10-13 11:02:14 +0200

    Added test data after initializing app

--- src/main/java/com/example/contentcalendar/Application.java
+++ src/main/java/com/example/contentcalendar/Application.java
@@ -1,13 +1,38 @@
 package com.example.contentcalendar;
 
+import com.example.contentcalendar.model.Content;
+import com.example.contentcalendar.model.Status;
+import com.example.contentcalendar.model.Type;
+import com.example.contentcalendar.repository.ContentRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	CommandLineRunner commandLineRunner(ContentRepository repository) {
+		return args -> {
+			// insert some data into the database
+			Content content = new Content(null,
+					"Hello, testing initial data in DB",
+					"All about Spring Boot...",
+					Status.IDEA,
+					Type.VIDEO,
+					LocalDateTime.now(),
+					null,
+					"");
+
+			repository.save(content);
+		};
 	}
 
 }
